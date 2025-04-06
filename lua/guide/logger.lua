1837ef61999358cdf3b8a888933ab6ec3228fbac
Eric Wong <wsdjeg@outlook.com>
2025-04-06 20:08:14 +0800

    add logger

---
+++ lua/guide/logger.lua
@@ -0,0 +1,19 @@
+local M = {}
+local logger
+
+local function log(t, msg)
+	if not logger then
+		local ok, l = pcall(require, "logger")
+		if ok then
+			logger = l.derive("guide")
+			logger[t](msg)
+		end
+	else
+		logger[t](msg)
+	end
+end
+function M.info(msg)
+	log("info", msg)
+end
+
+return M
