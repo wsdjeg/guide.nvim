17f2ab8c3805fb21598dd086a910bf68ac427840
Eric Wong <wsdjeg@outlook.com>
2025-04-21 23:08:29 +0800

    update

--- lua/keymap-guide/logger.lua
+++ lua/keymap-guide/logger.lua
@@ -2,18 +2,21 @@ local M = {}
 local logger
 
 local function log(t, msg)
-	if not logger then
-		local ok, l = pcall(require, "logger")
-		if ok then
-			logger = l.derive("guide")
-			logger[t](msg)
-		end
-	else
-		logger[t](msg)
-	end
+    if not logger then
+        local ok, l = pcall(require, 'logger')
+        if ok then
+            logger = l.derive('guide')
+            logger[t](msg)
+        end
+    else
+        logger[t](msg)
+    end
 end
 function M.info(msg)
-	log("info", msg)
+    log('info', msg)
+end
+function M.debug(msg)
+    log('debug', msg)
 end
 
 return M
